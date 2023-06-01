ad37c4460cd248424fb4c1563f53a2b57c4e8d5e
Camila <camilappadalino@gmail.com>
2023-06-01 16:06:33 -0300

    adicionando slideshow e validando cadastro js

--- script/script.js
+++ script/script.js
@@ -4,3 +4,19 @@ const secaoAlvo = document.getElementById('conteudoHome');
 paraScrollar.addEventListener('click', function() {
     secaoAlvo.scrollIntoView({ behavior: 'smooth', duration:1.8});
 });
+
+function validacaoCadastro(){
+    let nomeUsuarioCadastro = document.getElementById('nomeUsuarioCadastro').value 
+    let emailUsuarioCadastro = document.getElementById('emailUsuarioCadastro').value 
+    let cepUsuarioCadastro = document.getElementById('cepUsuarioCadastro').value
+    let ruaUsuarioCadastro = document.getElementById('ruaUsuarioCadastro').value 
+    let bairroUsuarioCadastro = document.getElementById('bairroUsuarioCadastro').value
+    let numeroUsuarioCadastro = document.getElementById('numeroUsuarioCadastro').value
+
+    if (nomeUsuarioCadastro == "" || emailUsuarioCadastro == "" || cepUsuarioCadastro == "" || ruaUsuarioCadastro == "" || bairroUsuarioCadastro == "" || numeroUsuarioCadastro == "") {
+        alert('Preencha todos campos antes de prosseguir')
+    } else {
+        alert("Seu cadastro foi efetivado!")
+    }
+}
+
